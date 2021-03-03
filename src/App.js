0161ec86f1e1b68ced15feec02089264a8a51f48
joanterm <jo_joanarg@yahoo.com>
2021-03-02 20:44:35 -0500

    Changed into a class component using state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import './App.css';
 import TodoItem from './components/TodoItem.js'
 import todoData from './components/TodoData.js'
 
-const App = () => {
 
-  console.log(todoData)
-  const todoComponents = todoData.map(item => <TodoItem key={item.id} completed={item.completed} dataText={item.text}/>)
+// const App = () => {
 
-  return(
-    <div className="todo-list">
-      {todoComponents}
-    </div>
-  )
+//   const todoComponents = todoData.map(item => <TodoItem key={item.id} completed={item.completed} dataText={item.text}/>)
+
+//   return(
+    // <div className="todo-list">
+    //   {todoComponents}
+    // </div>
+//   )
+// }
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+  }
+  render() {
+    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} completed={item.completed} dataText={item.text}/>)
+    return(
+      <div className="todo-list">
+        {todoComponents}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import React from "react"
 const TodoItem = (props) => { 
     return (
         <div className="todo-item">
-            {/* the below code will ensure that once the box is checked, it will be marked as completed and cannot be unchecked */}
+            {/* the below code + propert of checked will ensure that once the box is checked, it will be marked as completed and cannot be unchecked */}
             <input type="checkbox"checked={props.completed}/> 
             <p className="input-style">{props.dataText}</p>
         </div>
